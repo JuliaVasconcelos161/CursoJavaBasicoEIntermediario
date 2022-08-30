bfd7ab2baa07e5295871ef81178e4869eaac16d5
Júlia Vasconcelos <juliavasconceloscr@gmail.com>
2022-08-30 13:18:44 -0300

    Aula 7

---
+++ Aula07/Erro2.java
@@ -0,0 +1,9 @@
+class Erro2
+{
+	public static void main(String[] args)
+	{
+		int cont;
+		System.out.println("Você digitou " + cont);
+		//Erro semântica, a variável cont não foi inicializada
+	}
+}
---
+++ Aula07/Erro3.java
@@ -0,0 +1,9 @@
+class Erro3
+{
+	public static void Main(String[] args)
+	{
+		System.out.println("Você digitou");
+	}
+	//Erro em tempo de execução, não possui o método main
+	//Compila, mas não executa
+}

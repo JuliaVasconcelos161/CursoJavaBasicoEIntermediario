4a7f1667bd6e266564bd99d20c8d5daf8932a50d
Julia <juliavasconceloscr@gmail.com>
2022-12-03 20:44:36 -0300

    Utilizando jars e finalizando curso java intermediário

---
+++ eclipse/curso_java_basico/src/com/loiane/cursojava/aula85_100/UsandoJars.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula85_100;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class UsandoJars {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("loiane"));
+		
+
+	}
+
+}
